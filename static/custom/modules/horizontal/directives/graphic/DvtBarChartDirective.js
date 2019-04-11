9bad90f9c935e5f8fe7a15e3327bf0968386aede
ilorenzo10 <jilorenzo@bilbomatica.es>
2019-04-11 11:45:56 +0200

    Maximize chart modal window

--- static/custom/modules/horizontal/directives/graphic/DvtBarChartDirective.js
+++ static/custom/modules/horizontal/directives/graphic/DvtBarChartDirective.js
@@ -244,11 +244,10 @@ define(function (require) {
             + '<div class="chart--wrapper">'
                 + '<div data-ng-attr-id="{{ id }}"></div>'
             + '</div>'
-
-            + '<div class="legend-info" ng-if="isMaximized && legendClickMode && legend">Click on each value on the legend to hide/show in on the chart</div>'
-
-            + '<div ng-if="isMaximized && query != gauss" class="logoGraphics-wrapper"><img alt="European Agency for Safety and Health at Work" src="/pentaho/plugin/pentaho-cdf-dd/api/resources/system/osha-dvt-barometer/static/custom/img/osha-logo.svg" class="logoGraphics"></div>'
-
+            + '<div class="legend-text-block">'
+                + '<div ng-if="isMaximized && query != gauss" class="logoGraphics-wrapper"><img alt="European Agency for Safety and Health at Work" src="/pentaho/plugin/pentaho-cdf-dd/api/resources/system/osha-dvt-barometer/static/custom/img/osha-logo.svg" class="logoGraphics"></div>'
+                + '<div class="legend-info" ng-if="isMaximized && legendClickMode && legend">Click on each value on the legend to hide/show in on the chart</div>'
+            + '</div>'
             //+ '<div ng-if="!!functionalLegend" class="functionalLegend" data-ng-bind-html="functionalLegend"></div>'
 
         //+ '</div>';
