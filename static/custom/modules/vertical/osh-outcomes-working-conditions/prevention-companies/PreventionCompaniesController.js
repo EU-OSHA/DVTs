2fc9225a2c5f7c8b4b5d1682a1be12b8f0a78063
aprado94 <aprado@bilbomatica.es>
2019-08-01 14:18:43 +0200

    OSHB-179: Mobile: Work Accidents - The page reloads automatically when scrolling fast.

--- static/custom/modules/vertical/osh-outcomes-working-conditions/prevention-companies/PreventionCompaniesController.js
+++ static/custom/modules/vertical/osh-outcomes-working-conditions/prevention-companies/PreventionCompaniesController.js
@@ -55,8 +55,13 @@ define(function (require) {
     $scope.axisWordBreak = resolution > 768 ? 1 : '';
 
     $(window).on("resize",function(e){
-      resolution = screen.width;
-      $state.reload();
+      if(screen.width != resolution){
+        resolution = screen.width;
+        //$log.warn('Resolucion ha cambiado');
+        $state.reload();
+      }else{
+        //$log.warn('Resolucion no ha cambiado');
+      }
     });
 
     $scope.countriesDataFor = [];
