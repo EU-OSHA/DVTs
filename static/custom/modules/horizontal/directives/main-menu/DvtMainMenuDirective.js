e28880a29a29cf06425e61ac4eb814580c5e1c6e
ilorenzo10 <jilorenzo@bilbomatica.es>
2019-09-04 14:20:11 +0200

    click and touchstart events

--- static/custom/modules/horizontal/directives/main-menu/DvtMainMenuDirective.js
+++ static/custom/modules/horizontal/directives/main-menu/DvtMainMenuDirective.js
@@ -112,22 +112,22 @@ define(function (require) {
                     }
 
                     //angular.element('body').mouseup(function(e){
+                    /*
                     $(document).click(function(e) {
                       var container = angular.element('.filter--dropdown--wrapper');
-                      //console.log('desktop -------------------------' + e.target);
                       if (!container.is(e.target) && container.has(e.target).length === 0){
                         angular.element('.filter--dropdown--wrapper').removeClass('viewOptions'); 
                       }
                     });
-
-                    $(document).on('touchstart', function(e) {
+                    */
+                    $('body').on('click touchstart', function(e) {
                       var container = angular.element('.filter--dropdown--wrapper');
                       //console.log('movil -------------------------' + e.target);
                       if (!container.is(e.target) && container.has(e.target).length === 0){
                         angular.element('.filter--dropdown--wrapper').removeClass('viewOptions'); 
                       }
                     });
-
+                    
 
 
                     // when click banner link to Home
