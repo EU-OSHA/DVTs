df97b890cfbfb51767a8ded022b991f3d83eed65
dprado002 <dprado@bilbomatica.es>
2020-02-27 12:49:35 +0100

    Solved some issues with saving the country preferences

--- static/custom/modules/vertical/home/HomeController.js
+++ static/custom/modules/vertical/home/HomeController.js
@@ -48,14 +48,27 @@ define(function (require) {
         }
 
         $scope.saveCountry = function(e){
-          $(e.currentTarget).toggleClass('country-unlock').toggleClass('country-lock');
-
-          $rootScope.defaultCountry = {
-            code : $scope.pCountry1,
-            isCookie : 1
+          var removed = false;
+          if ($cookies.get("selectedCountry") != undefined)
+          {
+            $cookies.remove("selectedCountry");
+            removed = true;
           }
 
-          $cookies.put('selectedCountry', $scope.pCountry1);
+          $(e.currentTarget).toggleClass('country-unlock').toggleClass('country-lock');
+
+          if (removed == false)
+          {
+            $rootScope.defaultCountry = {
+              code : $scope.pCountry1,
+              isCookie : 1
+            }
+
+            if ($cookies.get('angular-consent.global'))
+            {
+              $cookies.put('selectedCountry', $scope.pCountry1);  
+            }
+          }          
         }
 
         $scope.EUData = {};
