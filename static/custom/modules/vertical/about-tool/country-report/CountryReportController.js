7a957e8bd7e16fc6613ef2795f11bcde59012d53
ilorenzo10 <jilorenzo@bilbomatica.es>
2020-05-06 12:50:10 +0200

    working-conditions -- full country report

--- static/custom/modules/vertical/about-tool/country-report/CountryReportController.js
+++ static/custom/modules/vertical/about-tool/country-report/CountryReportController.js
@@ -284,8 +284,8 @@ define(function (require) {
 
 
 			var header = "<picture>";
-	      	header += '<img src="/pentaho/plugin/pentaho-cdf-dd/api/resources/system/osha-dvt-barometer/static/custom/img/EU-OSHA-trans-en.png" alt="European Agency for Safety and Health at Work">';
-	      	header += '<img src="/pentaho/plugin/pentaho-cdf-dd/api/resources/system/osha-dvt-barometer/static/custom/img/eu-flag.png" title="European Union flag">';
+	      	header += '<img src="/pentaho/plugin/pentaho-cdf-dd/api/resources/system/osha-dvt-barometer/static/custom/img/EU-OSHA-trans-en.png" class="osha-logo" alt="European Agency for Safety and Health at Work">';
+	      	header += '<img src="/pentaho/plugin/pentaho-cdf-dd/api/resources/system/osha-dvt-barometer/static/custom/img/eu-flag.png" class="european-flag" title="European Union flag">';
 	    		header += '</picture>';
 	    		$(" .header-logo").append( $(header) );
 
